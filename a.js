b51c68f98aaceafbf157227e80ceab60594c36f7
Muhammed Ashir <muhammedashir543@gmail.com>
2023-10-04 10:49:23 +0530

    nc

--- a.js
+++ a.js
@@ -23,3 +23,18 @@ function createRandomDrop() {
 }
 
 setInterval(createRandomDrop, 1000); // Create a new drop every 8 seconds
+
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
